b0ea80097e1adba9e917acd04cf29924d0e02a69
Athul Sanjose <59477486+UltimateRoman@users.noreply.github.com>
2020-09-05 18:14:16 +0530

    2nd commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Web3 from 'web3';
+import Navbar from './Navbar';
 import './App.css';
 
 
@@ -29,7 +30,7 @@ class App extends Component {
   render() {
     return (
       <Router>   
-        
+        <Navbar />
       </Router>
     );
   }
